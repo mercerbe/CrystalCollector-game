23cdc0ebe3b448751f768def41077b9a1a70118f
Admin <admin@Admins-MacBook-Pro-2.local>
2018-03-31 18:11:55 -0400

    updates to HTML, CSS and javaScript start

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,24 @@
 //Global Variables
+var wins = 0;
+var losses = 0;
+var gemSelections = null;
+var winnningScore;
+var crystalOne;
+var crystalTwo;
+var crystalThree;
+var crystalFour;
+
 
 //HTML
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("losses").innerHTML = losses;
+
 
 //Functions
+function startGame() {
 
-//Initialize Game 
+}
+
+
+//Initialize Game
+window.onload()
