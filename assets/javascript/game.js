bfaeb8e3a5d021a8114718ac41a37e74e9871344
Admin <admin@admins-mbp-2.lan>
2018-04-01 14:32:41 -0400

    Match score and crystal values added to javaScript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,16 +37,42 @@ var downloadTimer = setInterval(function timer(){
 //Generate Score btwn 19-120
 var generatedScore = Math.floor((Math.random() * 120) + 1);
 $("#generatedScore").append(generatedScore);
-console.log(generatedScore);
+console.log("Match: " + generatedScore);
 
 //Generate crystal values btwn 1-12
 var crystalOne = Math.floor((Math.random() * 12) + 1);
-$("#crystalOne").attr("value")
+$("#crystalOne").attr("value", crystalOne);
+console.log("Crystal One: " + crystalOne);
+
+var crystalTwo = Math.floor((Math.random() * 12) + 1);
+$("#crystalTwo").attr("value", crystalTwo);
+console.log("Crystal Two: " + crystalTwo);
+
+var crystalThree = Math.floor((Math.random() * 12) + 1);
+$("#crystalThree").attr("value", crystalThree);
+console.log("Crystal Three: " + crystalThree);
+
+var crystalFour = Math.floor((Math.random() * 12) + 1);
+$("#crystalFour").attr("value", crystalFour);
+console.log("Crystal Four: " + crystalFour);
+
 
 //User Score update
+$(".playButtonArea").on("click", "#crystalOne", function(){
+
+});
+
 
 //check win (if userScore = generatedScore)
-
+if (userScore === generatedScore) {
+  document.getElementById("wins").innerHTML = wins;
+  setTimeout(function(){alert("You Win!");}, 300);
+  wins++;
+} else if (userScore > generatedScore) {
+  document.getElementById("losses").innerHTML = losses;
+  setTimeout(function(){alert("You Lose!");}, 300);
+  losses++;
+}
 
 //Restart button
 $("#restartButton").on("click", function() {
